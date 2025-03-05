b148e06223e230ff375823fea5d1691cb477c4cf
elie-dev2 <eliemwandu00@gmail.com>
2025-03-05 10:46:15 +0100

    parti elie

---
+++ navbar.php
@@ -0,0 +1,35 @@
+<nav
+    class="navbar navbar-expand-sm navbar-light bg-light"
+>
+    <div class="container">
+        <a class="navbar-brand" href="#">Depence</a>
+        <button
+            class="navbar-toggler d-lg-none"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="categori.php">Catégorie</a>
+                    
+                </li>
+                <li class="nav-item">
+                    
+                    <a class="nav-link" href="depense.php">Dépences</a>
+                </li>
+                <li class="nav-item">
+                    
+                    <a class="nav-link" href="login.php">deconnexion</a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</nav>
